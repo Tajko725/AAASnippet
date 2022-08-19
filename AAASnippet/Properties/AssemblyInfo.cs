d83c3c45bda606ea661def69617a69af5a0736e1
Mariusz K <mariuszklap@wp.pl>
2022-08-20 01:18:20 +0200

    Add Demo.gif

--- AAASnippet/Properties/AssemblyInfo.cs
+++ AAASnippet/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AAASnipped")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A set of useful snippets to generate default  methods for xUnit (Test, Fact, theory, async), NUnit (Test, async) methods with AAA or just AAA (Arrange, Act, Assert) comments")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Tajko")]
 [assembly: AssemblyProduct("AAASnipped")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
